c776b1d171241c480d44d7f9a366b01c22e43361
Anthony Do <anthony4do@gmail.com>
2019-11-30 17:03:43 -0800

    Updates

---
+++ script.js
@@ -0,0 +1,25 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+socket.on('chat-message', data => {
+  appendMessage(data)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+
+  if (message != '') {
+    appendMessage(message)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+  }
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
